a54ce7c971d9142c3d22eebaebc5948494d05fd9
Mathieu Berube <mathieu.berube@gmail.com>
2012-03-21 12:16:58 -0400

    Add attr_accessible to attributes

--- app/models/openid_association.rb
+++ app/models/openid_association.rb
@@ -1,2 +1,3 @@
 class OpenidAssociation < OpenidAbstract
+  attr_accessible :server_url, :target, :handle, :secret, :issued_at, :lifetime, :assoc_type
 end
--- app/models/openid_nonce.rb
+++ app/models/openid_nonce.rb
@@ -1,4 +1,5 @@
 class OpenidNonce < OpenidAbstract
+  attr_accessible :target, :server_url, :timestamp
 
   # attempt to scan timestamps (integers) first for fast access.
   def self.exists_by_target?(timestamp, salt, target)
